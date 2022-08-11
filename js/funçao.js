97a7606653049d7d98324ee3cb938e7df420f619
mateus lucas <108986368+Mateuslucs@users.noreply.github.com>
2022-08-11 19:23:54 -0300

    atualização

--- js/funçao.js
+++ js/funçao.js
@@ -192,7 +192,7 @@ contato2.addEventListener('keypress', function(e){
 
 /*Inserindo dados no documento*/
 
-function doc(){
+function doc (){
 
     /*DADOS LOCADOR*/
 
@@ -693,3 +693,29 @@ function doc(){
     })
     
 }
+
+const fs = require('fs')
+const nome_arquivo = `/../contratos/caução.doc`
+const path = __dirname + `${nome_arquivo}`
+
+function leitorDeArquivo(caminho){
+    fs.readFile(caminho, 'utf-8', function(error,data){
+        if(error){
+            console.log('erro de leitura: '+error.message)
+        }else {
+            console.log(data)
+        }
+    })
+}
+
+function escritorDeArquivo(caminho,texto){
+    fs.writeFile(caminho, texto, function(error){
+        if(error){
+            console.log('erro de escrita'+ error.message)
+        }else {
+            console.log('escreveu com sucesso em '+ caminho)
+        }
+    })
+}
+
+escritorDeArquivo(path, 'ola mundo')
