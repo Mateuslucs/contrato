051f00ad8aad4fab5ec879457b3f16ed03b1d0c6
mateus lucas <108986368+Mateuslucs@users.noreply.github.com>
2022-10-11 17:49:28 -0300

    atualização do media print

--- php/documento.php
+++ php/documento.php
@@ -14,18 +14,44 @@
     /*$section->addText(
         '"Learn from yesterday, live for today, hope for tomorrow. '.'The important thing is not to stop questioning." '. '(Albert Einstein)'
     );*/
-    $html = "<h1>CONTRATO DE LOCAÇÃO</h1>
+
+    /*escrevendo com tags*/
+    /*$html = "<h1>CONTRATO DE LOCAÇÃO</h1>
     <h1>RESIDENCIAL</h1>";
     $html .= "<p><strong>INÍCIO:</strong> $diadoprazoin/$mesdoprazoin/$anodoprazoin</p>
     <p><strong>TERMINO:</strong> $diadoprazofim/$mesdoprazofim/$anodoprazofim</p>";
     
     \PhpOffice\PhpWord\Shared\Html::addHtml($section,$html,false,false);
 
-    $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
-    
+    $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');*/
 
-    /*dowload do documento*/
+    /*escrevendo normal com fontes*/
+
+    /*$fontStyle = new \PhpOffice\PhpWord\Style\Font();
+    $fontStyle->setBold(true);
+    $fontStyle->setName('Tahoma');
+    $fontStyle->setSize(13);
+    $myTextElement = $section->addText('I am styled by a paragraph style definition.', null, $paragraphStyleName);
+    $myTextElement->setFontStyle($fontStyle);*/
+
+   
+    $estilo = \PhpOffice\PhpWord\SimpleType\TextAlignment::CENTER;
+    $fontStyle = new \PhpOffice\PhpWord\Style\Font(); 
+    $fontStyle->setBold(true);
+    $section->addText('ola mundo', [$fontStyle], [$estilo]);
+    /*$fontStyle = new \PhpOffice\PhpWord\Style\Font();
+    $fontStyle->setBold(true);
+    $fontStyle->setName('Tahoma');
+    $fontStyle->setSize(13);
+    $fontStyle->setParagraph($estilo);
+    $myTextElement = $section->addText('"Believe you can and you\'re halfway there." (Theodor Roosevelt)');
+    $myTextElement->setFontStyle($fontStyle);*/
+
+    $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
+    $objWriter->save('helloWorld.docx');
+
+    /*dowload do documento
     header("Content-Type: application/octet-stream");
     header("Content-Disposition: attachment;filename=contrato.docx");
-    $objWriter->save("contrato.docx");
+    $objWriter->save("contrato.docx");*/
 ?>
